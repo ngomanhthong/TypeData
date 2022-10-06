04873f3ded8d71c86a446bd3112829d0296af983
ngomanhthong <113655638+ngomanhthong@users.noreply.github.com>
2022-10-06 10:56:56 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,49 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		String ten = "Ngo Manh Thong";
+		String maSinhVien = "03665465";
+		String cmnd = "03264546485";
+		String tenLop = "Tin Hoc Ung Dung";
+		String tenTruong = "HUEIC";
+		float diemTrungBinh = 9.4F;
+
+		System.out.println("Thong Tin Mot Sinh Vien: ");
+		System.out.println("Ten: " + ten);
+		System.out.println("Ma Sinh Vien: " + maSinhVien);
+		System.out.println("CMND: " + cmnd);
+		System.out.println("Ten Lop: " + tenLop);
+		System.out.println("Ten Truong: " + tenTruong);
+		System.out.println("Diem Trung Binh: " + diemTrungBinh);
+
+		System.out.println("-------");
+
+		String tenCongDan = "Ngo Manh Thong";
+		String diaChi = "6/15 An Thuong Cong chua";
+		String sdt = "0392872318";
+		String heSoLuong = "7.000.000 vnd";
+		
+		System.out.println("Thong Tin Cong Nhan: ");
+		System.out.println("Ten Cong Nhan: " + tenCongDan);
+		System.out.println("Dia Chi: " + diaChi);
+		System.out.println("SDT: " + sdt);
+		System.out.println("He So Luong: " + heSoLuong);
+		
+		System.out.println("-------");
+		
+		String tenXe = "Mercedes";
+		String hangXe = "Mercedes-Benz";
+		String giaXe = "1.6 ty";
+		String mauSon = "White";
+		
+		System.out.println("Thong Tin Mot Chiec Xe Car");
+		System.out.println("Ten Xe: " + tenXe);
+		System.out.println("Hang Xe: " + hangXe);
+		System.out.println("Gia Xe: " + giaXe);
+		System.out.println("Mau Son: " + mauSon);
+		
+
+	}
+
+}
